d6104acafbd9d202f58cdcd33fb720590ccb75a3
Youri <Youri@WOL>
2023-02-13 17:20:58 +0100

    Add project files.

---
+++ Database-website-yml/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Database_website_yml.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
