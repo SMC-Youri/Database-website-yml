0e4d96a52a973eaa555f81c2430fde08f67f6f5c
Youri Louis <youri@louis.ms>
2023-06-05 11:34:34 +0200

    Validation in contactpagina afgemaakt

--- Database-website-yml/Models/Person.cs
+++ Database-website-yml/Models/Person.cs
@@ -9,11 +9,14 @@ namespace Database_website_yml.Models
         [Required(ErrorMessage = "Gelieve uw achternaam in te vullen.")]
         public string lastname { get; set; }
         [EmailAddress(ErrorMessage = "Geen geldig Emailadres.")]
-        [Required(ErrorMessage = "Gelieve uw Emailadres in te vullen")]
+        [Required(ErrorMessage = "Gelieve uw emailadres in te vullen")]
         public string email { get; set; }
+        [Phone(ErrorMessage = "Geen geldig telefoonnummer")]
         [Required(ErrorMessage = "Gelieve uw telefoonnummer in te vullen")]
         public string phone { get; set; }
+        [Required(ErrorMessage = "Gelieve uw adres in te vullen")]
         public string address { get; set; }
-        public string description { get; set; }
+        [Required(ErrorMessage = "Gelieve een bericht achter te laten")]
+        public string message { get; set; }
     }
 }
