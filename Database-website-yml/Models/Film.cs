cf18d9ba84294ad57b1b287ec38be57bb1b5641a
Youri Louis <youri@louis.ms>
2023-06-14 10:59:25 +0200

    Added Cast & Crew section

--- Database-website-yml/Models/Film.cs
+++ Database-website-yml/Models/Film.cs
@@ -7,14 +7,26 @@
         public string description;
         public string age;
         public string trailer;
+        public string actor1;
+        public string actor2;
+        public string actor3;
+        public string name1;
+        public string name2;
+        public string name3;
 
-        public Film(string name, string image, string description, string age, string trailer)
+        public Film(string name, string image, string description, string age, string trailer, string actor1, string actor2, string actor3, string name1, string name2, string name3)
         {
             this.name = name;
             this.image = image; 
             this.description = description;
             this.age = age;
             this.trailer = trailer;
+            this.actor1 = actor1;
+            this.actor2 = actor2;
+            this.actor3 = actor3;
+            this.name1 = name1;
+            this.name2 = name2;
+            this.name3 = name3;
         }
     }
 }
